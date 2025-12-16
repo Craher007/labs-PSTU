31df29e8c3875c22e0181a4fa67f9a6541bb0698
Andrey <Andreylygalov7@gmail.com>
2025-12-16 15:00:43 +0500

    Схемы, ошибки

---
+++ sem_1/2_7/2_7_1/2_7_1.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cin >> n;
+    int k = sqrt(n);
+    if (k == sqrt(n)) {
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                cout << "*";
+            }
+            cout << endl;
+        }
+    }
+}
