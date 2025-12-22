af50f6daa8b356d37b204a3ba3f762672d270736
Andrey <Andreylygalov7@gmail.com>
2025-12-22 22:48:24 +0500

    eq_Iter

---
+++ sem_1/eq_Iter/eq_Iter.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float x, xPrev; // [2;4]
+    double eps = 0.000001;
+    double lambda = -0.1;
+    x = 4;
+    xPrev = 0;
+    while (abs(x - xPrev) > eps) {
+        xPrev = x;
+        x = xPrev+ lambda * (3 * x - 4 * log(x) - 5);
+    }
+    cout << "Корень по методу итераций: " << x << endl;
+}
