71ed0ac0bc0754afd3faee1d100f8a563985f141
Andrey <Andreylygalov7@gmail.com>
2025-12-23 14:10:09 +0500

    README + eq

---
+++ sem_1/eq_Half/eq_Half.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double a, b, c;
+    double fA, fB, fC;
+    double eps = 0.000001;
+
+    a = 2;
+    b = 4;
+    while ((b - a) > eps) {
+        c = (a + b) / 2;
+        fA = 3 * a - 4 * log(a) - 5;
+        fB = 3 * b - 4 * log(b) - 5;
+        fC = 3 * c - 4 * log(c) - 5;
+
+        if (fA * fC < 0) { b = c; }
+        else if (fC * fB < 0) { a = c; }
+        else { cout << "Error" << endl; }
+    }
+    cout << "Kor: " << a << "; " << b << endl;
+
+}
---
+++ sem_1/eq_Newton/eq_Newton.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float x, xPrev;
+    x = 2; 
+    xPrev = 0; 
+    double eps = 0.000001;
+    while (abs(x - xPrev) > eps) {
+        xPrev = x;
+        x = xPrev - (3 * x - 4 * log(x) - 5) / (3 - 4 / x);
+
+    }
+    cout << "Корень: " << x << endl;
+
+
+}
