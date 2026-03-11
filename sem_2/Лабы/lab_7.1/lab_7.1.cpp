bf82447d158fad30d7a313ce1c7daefc98f74403
Andrey <Andreylygalov7@gmail.com>
2026-03-11 11:44:58 +0500

    README + Задачи

---
+++ sem_2/Лабы/lab_7.1/lab_7.1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <complex>
+
+using namespace std;
+
+float multi(float a, float b) {
+    return a * b;
+}
+complex<double> multi(complex<double> a, complex<double> b) {
+    return a * b;
+}
+
+
+int main()
+{
+    complex<double> a1=(3,6);
+    complex<double> b1 = (6, 2);
+    float a = 5.3, b = 5.2;
+    cout << multi(a, b) << endl;
+    cout << multi(a1, b1);
+    
+    
+}
---
+++ sem_2/Лабы/lab_7.2/lab_7.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cstdarg>
+using namespace std;
+int sum(int k, ...) {
+    va_list args;
+    va_start(args, k);
+
+    int one = va_arg(args, int),f=0;
+    for (int i = 0; i < k - 1; i++) {
+        int two = va_arg(args, int);
+        if (i % 2 == 0) {
+            f += one * two;
+        }
+        else {
+            f -= one * two;         
+        }
+        one = two;
+
+    }
+    cout << endl;
+    return f;
+
+}
+
+int main()
+{
+    cout<<sum(5, 1, 2, 3, 4, 5)<<endl;
+    cout << sum(10, 1, 2, 3, 4, 5, 8, 3, 7, 2, 7) << endl;
+    cout << sum(12, 1, 2, 3, 4, 5, 8, 3, 7, 2, 7, -2, 1) << endl;
+}
