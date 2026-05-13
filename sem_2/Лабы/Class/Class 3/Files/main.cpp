6c944d71ced53a0d5f73c9b38feb19089bf255ca
Andrey <Andreylygalov7@gmail.com>
2026-05-13 09:52:41 +0500

    Class 3 files

---
+++ sem_2/Лабы/Class/Class 3/Files/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "Header.h"
+
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    Time a;
+    Time b;
+    Time c;
+    cout << "Введите минуты и секунды: "<<endl;
+    cin >> a;
+    cout << "Введите минуты и секунды: " << endl;
+    cin >> b;
+    c = a - b;
+    if (a == b) cout << "Интерваллы равны: ";
+    if (a != b)cout << "Интервалы не равны: ";
+
+    cout << a;
+    cout << b;
+    cout << c;
+
+}
+
